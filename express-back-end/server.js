4ce5ba943c983f55cc844481a10b69e253816af0
ghanbari <ghanbari@ualberta.ca>
2020-10-07 23:16:12 -0600

    refractored

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,34 +1,24 @@
-// const Express = require('express');
-// const App = Express();
-// const BodyParser = require('body-parser');
-// const PORT = 8080;
-
-// // Express Configuration
-// App.use(BodyParser.urlencoded({ extended: false }));
-// App.use(BodyParser.json());
-// App.use(Express.static('public'));
-
-// // Sample GET route
-// App.get('/api/data', (req, res) => res.json({
-//   message: "Seems to work!",
-// }));
-
-// App.listen(PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
-// });
 
 
 const Express = require('express');
 const App = Express();
+const compression = require('compression')
+const path = require('path')
+const morgan = require('morgan')
 const fs = require('fs');
 const BodyParser = require('body-parser');
+
 const PORT = 8080;
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
+
+
+
+
+
 
 // this function generates random number
 function getRandomIndex(playerArray) {
@@ -65,6 +55,8 @@ setInterval(() => {
   fs.writeFileSync('players.json', data);
   
 }, 4000);
+
+
 
 // Sample GET route
 
